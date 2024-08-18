c946666057b76292a77077d80ab3dcb2c4a8b6ac
yusuferens <yusuferenketen@gmail.com>
2024-08-19 00:17:33 +0300

    created funcitons essential for game logic

---
+++ index.js
@@ -0,0 +1,40 @@
+const cells = document.querySelectorAll(".cell")
+const statusText = document.querySelectorAll("#statusText")
+const restartBtn = document.querySelectorAll("#restartBtn")
+const winConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+]
+let options = ["","","","","","","","",""]
+let currentPlayer = "X";
+let running = false;
+
+function initializeGame(){
+
+}
+
+function cellClicked(){
+
+}
+
+function updateCell(cell, index){
+
+}
+
+function changePlayer(){
+
+}
+
+function checkWinner(){
+
+}
+
+function restartGame(){
+
+}
